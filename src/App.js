41448c0bf27d3d27e5dee6ff037289667ad525ea
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-09-27 18:38:11 +0300

    handling input

--- src/App.js
+++ src/App.js
@@ -14,15 +14,29 @@ export default class App extends React.Component {
       correctAnswer: Math.floor(Math.random() * 100) + 1
     };
   }
+
+  handleGuess(number) {
+    const { correctAnswer } = this.state
+    const diff = Math.abs(correctAnswer - number)
+    let newMessage = 'Cold'
+    if (diff < 20) {
+      newMessage = 'Warm'
+    }
+    if (diff < 10) {
+      newMessage = 'Hot'
+    }
+    this.setState({message:newMessage})
+  }
+
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">Hot and Cold Game</h1>
-          <h2>{this.state.message}</h2>
-          <GameForm clickHandler={this.handleGuess} />
-          <GuessNumber />
-          <GuessList />
-          <GuessTemperature />
+        <h1 className="App-title">Hot and Cold Game</h1>
+        <h2>{this.state.message}</h2>
+        <GameForm clickHandler={this.handleGuess.bind(this)} />
+        <GuessNumber />
+        <GuessList />
+        <GuessTemperature />
       </div>
     );
   }
